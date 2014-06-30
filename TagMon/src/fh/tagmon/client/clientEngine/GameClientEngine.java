9366934e2959ed5bf6f413152e1167a44fb41d42
Shinigamey <christian.schmid@hs-augsburg.de>
2014-06-30 15:41:30 +0200

    fixed client-connection

--- TagMon/src/fh/tagmon/client/clientEngine/GameClientEngine.java
+++ TagMon/src/fh/tagmon/client/clientEngine/GameClientEngine.java
@@ -6,8 +6,6 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
 import fh.tagmon.client.Helper_PlayerSettings;
 import fh.tagmon.client.gui.Fight;
@@ -16,51 +14,56 @@ import fh.tagmon.client.gui.ISetAbility;
 import fh.tagmon.gameengine.abilitys.Ability;
 import fh.tagmon.gameengine.gameengine.AbilityComponentList;
 import fh.tagmon.gameengine.player.PlayerInfo;
-import fh.tagmon.gameengine.helperobjects.ActionObject;
-import fh.tagmon.gameengine.helperobjects.AnswerObject;
-import fh.tagmon.gameengine.helperobjects.SummaryObject;
+import fh.tagmon.gameengine.helperobjects.*;
 import fh.tagmon.gameengine.player.MonsterPlayModule;
 import fh.tagmon.gameengine.player.deal_with_incoming_abilitys.AbilityComponentDirector;
 import fh.tagmon.network.ConnectionType;
-import fh.tagmon.network.clientConnections.ANetworkConnection;
-import fh.tagmon.network.clientConnections.NetworkSocketConnection;
-import fh.tagmon.network.message.MessageFactory;
-import fh.tagmon.network.message.MessageObject;
+import fh.tagmon.network.clientConnections.*;
+import fh.tagmon.network.message.*;
 
-public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Observer, ISetAbility{
+public class GameClientEngine extends AsyncTask <Void, Void, Void> implements ISetAbility{
 	
-	private Context context;
-    private final Object waitForPlayer;
-    private boolean wait = true;
-    private final Object waitForOtherDialog;
-    private boolean waitForDialog = false;
-    private ActionObject choosenAbility;
-    
-    //TODO durch zentralgespeicherte eigene player-ID ersetzen
-    private int ID = 0;
-    private List<PlayerInfo> players = null;
-
-    //GAMEPLAY
-	private MonsterPlayModule monster;
+		//GUI-HANDLING
+		private Context context;
+	    private final Object waitForPlayer;
+	    private boolean wait = true;
+	    private final Object waitForOtherDialog;
+	    private boolean waitForDialog = false;
+	    
+	    //USER-INFOS
+	    private int ID = 0;
+	    private List<PlayerInfo> players = null; //TODO should be replaced by saved user informations. -> database
 	
-	//NETWORK
-	private ANetworkConnection connection;
-    private ConnectionType type;
+	    //GAMEPLAY
+		private MonsterPlayModule monster;
+	    private ActionObject choosenAbility;
+		
+		//NETWORK
+		private ANetworkConnection connection;
+	    private ConnectionType connectionType;
 	
 	public GameClientEngine(Context context, MonsterPlayModule mPM, ConnectionType type){
-		this.context = context;
-		this.monster = mPM;
-        this.type = type;
-        waitForPlayer = new Object();
-        waitForOtherDialog = new Object();
+		this.context 		= context;
+		this.monster 		= mPM;
+        this.connectionType = type;
+        waitForPlayer 		= new Object();
+        waitForOtherDialog 	= new Object();
 	}
 
     @Override
     protected Void doInBackground(Void... params) {
-        connectToNetwork(type);
+        connectToNetwork(connectionType);
+		if(connection != null) 
+			listenToBroadcast();
 		return null;
     }
 
+    /**
+     * This method is ment to establish a network-connection based on the connectiontype.
+     * Connectiontypes, which are planed to be supported are:
+     * Bluetooth, TCP-Socket-Connection, Localhost-Connection (TPC-Socket).
+     * @param ConnectionType type
+     */
     private void connectToNetwork(ConnectionType type){
 		switch(type){
 		case BLUETOOTH:
@@ -68,8 +71,6 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
 		case LCL_SOCKET:
 			try {
 				connection = new NetworkSocketConnection("localhost");
-
-			//	t = new Thread(connection);
 	        } catch (IOException e) {
 	            Log.e("Client localhost connection", e.getMessage());
 	            connection = null;
@@ -80,23 +81,29 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
 		default:
 			break;
 		}
-		if(connection != null){
-		    listenToBroadcast();
-		}
     }
 
-
+    private boolean listeningToBroadcast = true;
+    /**
+     * This method uses the previously established network connection as long as the member-variable "listenToBroadcast"
+     * remain true. Right after that have happened, listenToBroadcast() closes the opened connection.
+     * <br/>
+     * Each received Host-Message is proceeded to the handleInput(MessageObject<?> msg)-method.
+     */
     private void listenToBroadcast(){
-        boolean running = true;
-        while(running) {
+        while(listeningToBroadcast) {
             MessageObject<?> obj= connection.listenToBroadcast();
-            update(null, obj);
+            handleInput(obj);
         }
+        connection.closeConnection();
     }
 
-	@Override
-	public void update(Observable observable, Object hostMsg) {
-		MessageObject<?> msg = (MessageObject<?>) hostMsg;
+    /**
+     * handleInput is called by listenToBroadcast() whenever a host-message was received. The handleInput(msg)
+     * just delegates the message in order to guarantee its correct handling.
+     * @param msg
+     */
+	private void handleInput(MessageObject<?> msg) {
         switch(msg.messageType){
         case GAME_START:
             proceedGameStart(msg);
@@ -119,6 +126,9 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
 		}
 	}
 	
+	/**
+	 * This method is called one before the actually game begin. Its neccesary to prepare mandatory data.
+	 */
 	private void proceedGameStart(MessageObject<?> msg){
 		this.ID = (Integer) msg.getContent();
         PlayerInfo startInfo = new PlayerInfo(Helper_PlayerSettings.playerName, ID);
@@ -127,6 +137,12 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
         this.connection.sendToHost(MessageFactory.createClientMessage_GameStart(startInfo, 0));
 	}
 	
+	/**
+	 * This method is called right before the first turn is done and right after that, whenever the 
+	 * current Turn is finished. The in messageobject enclosed summaryobject contains updated 
+	 * user-informations of each and every player, who was influenced by the currently handled
+	 * monster-ability.
+	 */
 	private void proceedSummary(MessageObject<?> msg){
 		if(this.players == null)
 			prepareFightGUI((List <PlayerInfo>) ((SummaryObject)msg.getContent()).getPlayerInfos());
@@ -142,10 +158,18 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
                 } catch (InterruptedException e) { }
             }
         }
-        ((Fight)context).showTemporaryDialog(summary.getSummary(), this);
+        String summaryMsg = summary.getSummary();
+        if(!summaryMsg.isEmpty())
+        	((Fight)context).showTemporaryDialog(summary.getSummary(), this);
 
         onPauseDialog();
 	}
+	
+	/**
+	 * This method is called only once before the first turn is handled.
+	 * It is ment to establish the monster's abilities and initializes the GUI.
+	 * @param players
+	 */
 	private void prepareFightGUI(List <PlayerInfo> players){
 		this.players = players;
         List<Ability> abilitylist = monster.getMonster().getAbilitys();
@@ -153,11 +177,17 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
 	}
     /**
      * Call this on pause.
+     * @author Pascal
      */
     public void onPauseDialog() {
         synchronized (waitForOtherDialog){ waitForDialog = true; }
     }
 	
+    /**
+     * proceedYourTurn(MessageObject<?> msg) handles the host-requests, which are sent 
+     * whenever the player is ment to choose a monster-ability.
+     * @param msg
+     */
 	private void proceedYourTurn(MessageObject<?> msg){
 		Log.d("your Turn", "player turn");
         this.monster.newRound(players, ID);
@@ -168,10 +198,15 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
 	}
     /**
      * Call this on pause.
+     * @author Pascal
      */
     public void onPause() {
         synchronized (waitForPlayer){ wait = true; }
     }
+    /**
+     * @author Pascal
+     * @return actionObject
+     */
 	private ActionObject waitForAction() {
         synchronized (waitForOtherDialog) {
             while (waitForDialog) {
@@ -193,6 +228,12 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
         return action;
     }
 	
+	/**
+	 * This method is called whenever one of your own monster's abilitys or any other monster's abilities affects
+	 * the client. The method delegates the contained abilitycomponentList to the MonsterAbilityComponentDirector
+	 * and expects an AnswerObject in return, which is sent back to the hist right after that.
+	 * @param msg
+	 */
 	private void proceedIncomingAbilityComponent(MessageObject<?> msg){
 		AbilityComponentDirector director = monster.getMonstersAbilityComponentDirector();
 		AbilityComponentList abilityComponents = (AbilityComponentList) msg.getContent();
@@ -201,17 +242,23 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
 		connection.sendToHost(MessageFactory.createClientMessage_Answer(answerObject, ID));
 	}
 	
+	/**
+	 * Displays the "Game finished"-Screen
+	 */
 	private void stop(){
         ((Fight) context).handleGameOver("Game finished", this);
 	}
 
+	/**
+	 * stops the entire client
+	 */
     public void closeGame() {
-        connection.deleteObservers();
-        connection.closeConnection();
+        listeningToBroadcast = false;
     }
 	
-
-
+    /**
+     * @author Pascal
+     */
     @Override
     public void setAbility(ActionObject actionObject) {
         choosenAbility = actionObject;
@@ -219,6 +266,7 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
     }
     /**
      * Call this on resume.
+     * @author Pascal
      */
     public void onResume() {
         synchronized (waitForPlayer) {
@@ -230,6 +278,7 @@ public class GameClientEngine extends AsyncTask <Void, Void, Void> implements Ob
 
     /**
      * Call this on resume.
+     * @author Pascal
      */
     public void onResumeDialog() {
         synchronized (waitForOtherDialog) {
